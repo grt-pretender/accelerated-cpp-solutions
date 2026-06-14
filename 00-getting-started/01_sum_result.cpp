c757132b4baf3df52c8d83ac7576e9e2469897bf
Daria Mashkova <dmetafrasis@gmail.com>
2026-06-14 18:42:02 +0700

    Add files

---
+++ 00-getting-started/01_sum_result.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+
+int main() {
+	std::cout << "What`s the result of 3 + 4?" << std::endl;
+	std::string s1 = "3";
+	std::string s2 = "4";
+
+	std::cout << "Combine two integers: " << 3 + 4 << std::endl;
+	std::cout << "Combine two strings: " << s1 + s2 << std::endl;
+	return 0;
+}
---
+++ 00-getting-started/03_tab.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+	std::cout << "This (\t) is a tab character." << std::endl;
+	return 0;
+}
